9604364a12c2bc86b7171b84d9b2c6b7ef29c8e3
favadkhan <favadkhan@rediff.com>
2017-02-27 16:12:43 -0800

    Add files via upload

---
+++ vmMemFree.sh
@@ -0,0 +1,4 @@
+echo "$(date '+%Y-%m-%d %H:%M:%S') $(free -m | grep Mem: | sed 's/Mem://g')"|awk '{print $1,$2,$3,$4,$5,$6,$7,$8}' >> /tmp/memoryfree
+mysql -u root -D "charts" -e "LOAD DATA INFILE 'memoryfree' into table VM_stats FIELDS TERMINATED BY ' '";
+mysql -u root -pHitachi1 -D "charts" -e "select * from VM_stats";
+rm -rf /tmp/memoryfree
