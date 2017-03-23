133e716a839c4ff82798023c188215df6a64f0bf
favadkhan <favadkhan@rediff.com>
2017-03-23 02:23:13 -0700

    Update dockerData.sh

--- dockerData.sh
+++ dockerData.sh
@@ -1,3 +1,3 @@
 docker stats --no-stream $(docker ps | awk '{if(NR>1) print $NF}')|sed -E "s/\// /g"|sed -E "s/\%/ /g"|awk '{print $1,$2,$7}'|awk 'NR>1' >> docker_stats_outputdata
-mysql -u monitoring -pHitachi1 --local-infile -D charts -e "LOAD DATA LOCAL INFILE 'docker_stats_outputdata' into table docker_stats_data FIELDS TERMINATED BY ' '"
-mysql -u monitoring -pHitachi1 -D charts -e "select * from docker_stats_data"
+mysql -u monitoring -pRequest1 --local-infile -D charts -e "LOAD DATA LOCAL INFILE 'docker_stats_outputdata' into table docker_stats_data FIELDS TERMINATED BY ' '"
+mysql -u monitoring -pRequest1 -D charts -e "select * from docker_stats_data"
