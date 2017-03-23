bd7d9c8038540edf147769bda9a841f308498018
favadkhan <favadkhan@rediff.com>
2017-03-23 02:22:31 -0700

    Update HSASIMemoryFreeData.sh
    updated user from Hitachi1 to Request1

--- HSASIMemoryFreeData.sh
+++ HSASIMemoryFreeData.sh
@@ -1,5 +1,5 @@
 echo "$(date '+%Y-%m-%d %H:%M:%S') $(top -c -b -n 5 | grep si.war | awk '{print $5,$6,$7,$9,$10}')" > HsaSiProcessesTopStatsData
-mysql -u monitoring -pHitachi1 --local-infile -D charts -e "LOAD DATA LOCAL INFILE 'HsaSiProcessesTopStatsData' into table Si_Top_Stats FIELDS TERMINATED BY ' '";
-mysql -u monitoring -pHitachi1 -D charts -e "select * from Si_Top_Stats";
+mysql -u monitoring -pRequest1 --local-infile -D charts -e "LOAD DATA LOCAL INFILE 'HsaSiProcessesTopStatsData' into table Si_Top_Stats FIELDS TERMINATED BY ' '";
+mysql -u monitoring -pRequest1 -D charts -e "select * from Si_Top_Stats";
 rm -f HsaSiProcessesTopStatsData
 cat HsaSiProcessesTopStatsData
